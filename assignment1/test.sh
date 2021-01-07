3fa776f661140c4a6115ad6b4e17ae855ae65dea
Sam Kamens <skamens1@fordham.edu>
2021-01-06 23:02:09 -0500

    Add script for assignment1

---
+++ assignment1/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+make hello_world
+
+if [ $? == 0 -a -f hello_world ]
+then
+    # Assume they did the C++ version.
+    output=`./hello_world`
+else
+    output=`./hello_world.py`
+fi
+
+case "$output" in 
+    "Hello, World!") 
+        echo "Test passed!"
+        exit 0
+        ;;
+    *) echo "Test failed!"
+        exit 1
+        ;;
+esac
+    
+
